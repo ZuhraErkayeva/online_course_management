1cf16bfa62a728a615eab8b1a857b6787e256218
Zuhra <erkayevazuhra6@gmail.com>
2024-11-28 17:13:43 +0300

    i'm done

--- api/models.py
+++ api/models.py
@@ -5,14 +5,24 @@ class Instructor(models.Model):
     email = models.EmailField()
     specialty = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
     instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name='instructor')
-    start_date = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
 
 class Lesson(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course')
+    order = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- api/serializers.py
+++ api/serializers.py
@@ -9,27 +9,32 @@ class InstructorSerializer(serializers.ModelSerializer):
         model = Instructor
         fields = ('id','name','email','specialty')
 
-    def validate_email(self, obj):
-        if Instructor.objects.filter(email=obj).exists():
+    def validate_email(self, email):
+        if Instructor.objects.filter(email=email).exists():
             raise serializers.ValidationError("Bu email manzili allaqachon mavjud.")
-        return obj
+        return email
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ('title', 'description', 'instructor', 'start_date', 'end_date')
 
-    def validate(self, obj):
-        if obj['start_date'] >= obj['end_date']:
+    def validate(self, non_field_errors):
+        start_date = non_field_errors.get('start_date')
+        end_date = non_field_errors.get('end_date')
+
+        if not start_date or not end_date:
+            raise serializers.ValidationError("Boshlanish va tugash sanalari kiritilishi shart.")
+        if start_date >= end_date:
             raise serializers.ValidationError("Kursning boshlanish sanasi tugash sanasidan oldin bo'lishi kerak.")
-        return obj
+        return non_field_errors
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = ('id', 'title', 'content', 'course', 'order')
 
-    def validate_order(self, obj):
-        if obj['order'] < 1:
+    def validate_order(self, order):
+        if order < 0:
             raise ValidationError("Tartib raqami musbat butun son bo'lishi kerak.")
-        return obj
+        return order
