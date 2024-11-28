ad198b1c21ada67c9d1b68f707405901967f0348
Zuhra <erkayevazuhra6@gmail.com>
2024-11-28 17:11:00 +0300

    I updated the urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import InstructorAPIView, CourseAPIView, LessonAPIView
+from .views import InstructorAPIView, CourseAPIView, LessonAPIView, InstructorDeleteView, InstructorUpdateView, CourseUpdateView,CourseDeleteView, LessonDeleteView, LessonUpdateView
 
 urlpatterns = [
-    path('instructor/', InstructorAPIView.as_view()),
-    path('course/', CourseAPIView.as_view()),
-    path('lesson/', LessonAPIView.as_view()),
+    path('instructors/', InstructorAPIView.as_view()),
+    path('courses/', CourseAPIView.as_view()),
+    path('lessons/', LessonAPIView.as_view()),
+    path('instructors/<int:pk>/update/', InstructorUpdateView.as_view()),
+    path('instructors/<int:pk>/delete/', InstructorDeleteView.as_view()),
+    path('courses/<int:pk>/update/', CourseUpdateView.as_view()),
+    path('courses/<int:pk>/delete/', CourseDeleteView.as_view()),
+    path('lessons/<int:pk>/update/', LessonUpdateView.as_view()),
+    path('lessons/<int:pk>/delete/', LessonDeleteView.as_view()),
 ]
